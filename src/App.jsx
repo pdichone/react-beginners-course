1194ffcf51597e091200370aaba73e5a8e8b76cd
Paulo <pdichone@gmail.com>
2024-05-13 21:00:51 -0700

    first code commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/** @format */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
+
+// Next:: Create a book list component
+// then:: show all the books on a ul>li
+function HelloWorld() {
+  return <p>Hello World, Hanna!</p>;
+}
