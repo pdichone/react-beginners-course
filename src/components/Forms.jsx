59683f09f7df513e3e3613d4a9963cc3f0b30c5c
Paulo <pdichone@gmail.com>
2024-05-15 12:37:16 -0700

    controlled elements - getting the form state to the react component

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,19 +1,27 @@
 /** @format */
 
+import { useState } from 'react';
+
 function NameForm() {
+  const [name, setName] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('EV::', event);
+  }
   return (
     <div className="container mt-4">
-      <form onSubmit={(event) => {
-        event.preventDefault()
-         console.log('Hello')
-      }}>
+      <form onSubmit={handleSubmit}>
         <input
           className="form-control"
           type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Enter your name"
         />
         <button className="btn btn-primary mt-2">Submit</button>
       </form>
+      <p>{name}</p>
     </div>
   );
 }
