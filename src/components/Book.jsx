fc8b77455139974f0a23509f4583e8fcec666eb1
Paulo <pdichone@gmail.com>
2024-05-14 20:47:48 -0700

    add state variable - the wrong way in React

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,8 @@
 /** @format */
 
 function Book({ title, author, description, subtitle, image }) {
+  var isRead = false;
+
   return (
     <div className="user-profile">
       <img src={image} alt={title} />
@@ -8,6 +10,14 @@ function Book({ title, author, description, subtitle, image }) {
       <h4>Subtitle: {subtitle}</h4>
       <h3>By {author}</h3>
       <p>Description: {description}</p>
+      <button
+        onClick={() => {
+          isRead = !isRead;
+          console.log('Status', isRead);
+        }}
+      >
+        {isRead ? 'Read' : 'Unread'}
+      </button>
     </div>
   );
 }
