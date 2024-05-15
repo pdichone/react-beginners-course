7222bf95c76d02f8cd3931e74f0b4e89062be7cc
Paulo <pdichone@gmail.com>
2024-05-15 14:25:14 -0700

    setup handleChange and setFormData

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -3,35 +3,69 @@
 import { useState } from 'react';
 
 function NameForm() {
-  const [name, setName] = useState('');
-  const [names, setNames] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    profession: '',
+  });
+  const [profiles, setProfiles] = useState([]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-
-    setNames([...names, name]);
-
-    setName('');
+    console.log('Event:::', event.target);
   }
   return (
     <div className="container mt-4">
       <form onSubmit={handleSubmit}>
-        <input
-          className="form-control"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Enter your name"
-        />
+        <div className="form-group mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Enter your name"
+          />
+        </div>
+
+        <div className="form-group mb-3">
+          <input
+            className="form-control"
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            placeholder="Enter age"
+          />
+        </div>
+
+        <div className="form-group mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="profession"
+            value={formData.profession}
+            onChange={handleChange}
+            placeholder="Enter profession"
+          />
+        </div>
         <button className="btn btn-primary mt-2">Submit</button>
       </form>
-      <ul>
+      {/* <ul>
         {names.map((nameItem, index) => (
           <li key={index} className="list-group-item">
             {nameItem}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
